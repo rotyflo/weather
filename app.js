956fdf2e5702332e7780afce7bdf167d02a37e41
RT <me@rtflores.com>
2021-09-22 17:24:49 -0700

    add server code

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+
+app.get("", (req, res) => {
+	res.send("hello world");
+});
+
+app.listen(PORT, () => {
+	console.log(`app up at port ${PORT}`);
+});
