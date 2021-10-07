c37749bdb642c66d945f5de7aa9b7952af823d4b
RT <me@rtflores.com>
2021-10-07 15:33:35 -0700

    finish version 0.1.0 lol, finally works

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
 $(document).ready(function () {
-  var geo = navigator.geolocation;
-  var icons = {
-    "01d": "<i class='wi wi-day-sunny'></i>",
-    "02d": "<i class='wi wi-day-cloudy'></i>",
-    "03d": "<i class='wi wi-cloud'></i>",
-    "04d": "<i class='wi wi-cloudy'></i>",
-    "09d": "<i class='wi wi-rain'></i>",
-    "10d": "<i class='wi wi-day-rain'></i>",
-    "11d": "<i class='wi wi-thunderstorm'></i>",
-    "13d": "<i class='wi wi-snow'></i>",
-    "50d": "<i class='wi wi-dust'></i>",
-    "01n": "<i class='wi wi-night-clear'></i>",
-    "02n": "<i class='wi wi-night-alt-cloudy'></i>",
-    "03n": "<i class='wi wi-cloud'></i>",
-    "04n": "<i class='wi wi-cloudy'></i>",
-    "09n": "<i class='wi wi-rain'></i>",
-    "10n": "<i class='wi wi-night-alt-rain'></i>",
-    "11n": "<i class='wi wi-thunderstorm'></i>",
-    "13n": "<i class='wi wi-snow'></i>",
-    "50n": "<i class='wi wi-dust'></i>"
-  }
-
-  // Initialize location to Seattle
-  getWeather("2490383");
-
+  let geo = navigator.geolocation;
+  let output = document.getElementById("output");
+  
   // Get weather for current location
   if (geo) {
     geo.getCurrentPosition(function (position) {
@@ -33,34 +11,60 @@ $(document).ready(function () {
     });
   }
 
-  function getWeather(woeid) {
-    let url = `https://goweather.herokuapp.com/weather/Seattle`;
+  let query = document.getElementById("query");
+  let submitButton = document.getElementById("submit-button");
 
-    fetch(url, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      credentials: "include"
-    })
-      .then(response => response.json())
-      .then(data => console.log(data));
+  submitButton.addEventListener("click", () => {
+    getLatLon(query.value);
+  });
 
-    // $.getJSON(url, function (api) {
-    //   console.log(JSON.stringify(api));
-    //   let icon = icons[api.weather[0].icon];
-    //   let description = api.weather[0].description;
-    //   let city = api.title;
-    //   let country = api.sys.country;
-    //   let temp = Math.round(api.main.temp);
+  query.addEventListener("keydown", (key) => {
+    if (key.code === "Enter") {
+      getLatLon(query.value);
+    }
+  });
 
-    //   $("#title").html(`L${icon}cal Weather`);
-    //   $("#location").html(`${city}, ${country}`)
-    //   $("#description").html(`${description}`);
-    //   $("#celcius").html(temp);
-    //   $("#fahrenheit").html(Math.round(temp * 9 / 5 + 32));
-    // });
+  function getLatLon(city) {
+    let url = `https://nominatim.openstreetmap.org/search.php?q=${city}&format=jsonv2`;
 
+    $.getJSON(url, (data) => {
+      let lat = data[0].lat;
+      let lon = data[0].lon;
 
+      getForecastURL(lat, lon);
+    });
+  }
+
+  function getForecastURL(lat, lon) {
+    let url = `https://api.weather.gov/points/${lat},${lon}`;
+
+    $.getJSON(url, (data) => {
+      getWeather(data.properties.forecast);
+    });
+  }
+
+  function getWeather(url) {
+    $.getJSON(url, (data) => {
+      let periods = data.properties.periods;
+
+      output.innerHTML = "";
+
+      for (let i = 0; i < periods.length; i++) {
+        let period = data.properties.periods[i];
+        let name = period.name;
+        let forecast = period.detailedForecast;
+        let icon = period.icon;
+
+        output.innerHTML += `
+          <hstack spacing=s align-y=top>
+          <img src="${icon}">
+          <p>
+            <b><u>${name}</u></b><br>
+            <span>${forecast}</span><br>
+          </p>
+          </hstack>
+        `;
+      }
+    });
   }
 });
